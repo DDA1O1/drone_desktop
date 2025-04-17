2dfe097c254c93945bf1045517901c1ef9c0cb8d
debashish <debashishdash03412@gmail.com>
2025-04-17 17:06:26 +0530

    Refactor Vite configuration to optimize dependency inclusion
    - Moved 'ws' from rollupOptions.external to optimizeDeps.include for improved clarity and performance.
    - Streamlined the configuration structure to enhance readability.

--- vite.main.config.mjs
+++ vite.main.config.mjs
@@ -2,11 +2,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      external: [
-        'ws', 
-      ]
-    }
+  optimizeDeps: {
+    include: ['ws']
   }
 });
